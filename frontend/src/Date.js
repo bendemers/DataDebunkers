5d6253ab45bba2ab259f9674216f934c33052b01
ScoutOK <ohkeefe@gmail.com>
2017-04-22 14:43:03 -0400

    functional date slider

--- frontend/src/Date.js
+++ frontend/src/Date.js
@@ -1,13 +1,15 @@
 import React, {Component} from 'react';
 import {Range} from 'rc-slider';
 
+import Label from './Label.js'
+
 export default class DateSlider extends Component {
   constructor () {
     super()
 
     this.state = {
       start: 0,
-      end: 24,
+      end: 59,
     }
 
     this.setAll = this.setAll.bind(this);
@@ -17,22 +19,22 @@ export default class DateSlider extends Component {
   setAll () {
     this.setState({
       start: 0,
-      end: 24
+      end: 59
     })
-    const handles = document.querySelectorAll('#time .rc-slider-handle');
+    const handles = document.querySelectorAll('#date .rc-slider-handle');
     handles[0].style.left = '0%';
     handles[1].style.left = '100%';
-    const track = document.querySelector('#time .rc-slider-track');
+    const track = document.querySelector('#date .rc-slider-track');
     track.style.left = '0%'
     track.style.width = '100%'
   }
 
   getPositions () {
-    const handles = document.querySelectorAll('#time .rc-slider-handle');
+    const handles = document.querySelectorAll('#date .rc-slider-handle');
     const place1str = handles[0].style.left.slice(0, -1)
-    const place1 = 24 * Number(place1str) / 100;
+    const place1 = Math.round(59 * Number(place1str) / 100);
     const place2str = handles[1].style.left.slice(0, -1)
-    const place2 = 24 * Number(place2str) / 100;
+    const place2 = Math.round(59 * Number(place2str) / 100);
     if (place1 < place2) {
       this.setState({
         start: place1,
@@ -48,22 +50,27 @@ export default class DateSlider extends Component {
   
   render () {
     return (
-      <div id='time' className='slider'>
-        <h2>Time of day</h2>
+      <div id='date' className='slider'>
+        <h2>Date</h2>
         <div className='interact'>
+          <div className='place-labels'>
+            <Label number={this.state.start}/>
+            <Label number={this.state.end}/>
+          </div>
           <Range 
-            defaultValue={[0,60]}
+            defaultValue={[0,59]}
             marks={{
               0: 'January \'11',
-              13: 'January \'12',
-              25: 'January \'13',
-              37: 'January \'14',
-              49: 'January \'15',
-              60: 'December \'15'
+              12: 'January \'12',
+              24: 'January \'13',
+              36: 'January \'14',
+              48: 'January \'15',
+              59: 'December \'15'
             }}
             min={0}
-            max={60}
+            max={59}
             step={1}
+            onChange={this.getPositions}
             onAfterChange={this.getPositions}
             />
             {/*<button onClick={this.setAll}>All Times</button>*/}
